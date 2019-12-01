4c48ebffff2e4ecfbd7b2ff5b38d4cec52d0914f
zltttt <zltttt@protonmail.com>
2019-12-01 16:12:51 +0800

    44.5

--- Task 6/2019201413曾乐天/main.c
+++ Task 6/2019201413曾乐天/main.c
@@ -8,7 +8,7 @@ extern void del(struct list*);
 
 int read(char *s)
 {
-	int x = 0;char c = *s;
+	int x = 0; char c = *s;
 	while(c < '0' || '9' < c)c = *++s;
 	while('0' <= c && c <= '9')x = x * 10 + (c ^ 48), c = *++s;
 	return x;
@@ -19,7 +19,7 @@ int main(int argc,char *argv[])
 	int type, n, k, i;
 	
 	if(argc == 1)
-		scanf("%d%d%d",&n,&k,&type);
+		scanf("%d%d%d", &n, &k, &type);
 	else if(3 <= argc && argc <= 4)
 	{
 		n = read(argv[1]);
@@ -28,21 +28,21 @@ int main(int argc,char *argv[])
 	}
 	else
 	{
-		fprintf(stderr,"Error: argument count not match\n");
+		fprintf(stderr, "Error: argument count not match\n");
 		return -1;
 	}
 
 	//type = 0:clockwise, type = 1:counerclockwise
 	struct list *start = newlist(1), *cur = start;
-	for(i = 2;i <= n; i++)
+	for(i = 2; i <= n; i++)
 	{
-		link(cur,newlist(i));
+		link(cur, newlist(i));
 		cur = move(cur, 0);
 	}
-	link(cur,start);
+	link(cur, start);
 	
 	cur = start;
-	for(;n>1;n--)
+	for(; n>1; n--)
 	{
 		for(i = 1; i <= k; i++)
 			cur = move(cur, type);
@@ -51,7 +51,7 @@ int main(int argc,char *argv[])
 		cur = move(cur, type);
 		del(move(cur, type ^ 1));
 	}
-	printf("%d\n",value(cur));
+	printf("%d\n", value(cur));
 
 	return 0;
 }
