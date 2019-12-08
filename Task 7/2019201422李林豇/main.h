d828a6b0c32f2bc958a6c35d57573bf6eaf74027
nicodafagood <nicodafagood@LAPTOP-LKJ6HH54.localdomain>
2019-12-09 00:44:55 +0800

    带 作 业 7

---
+++ Task 7/2019201422李林豇/main.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include"struct.h"
+
+extern Treap* Treap_build(cmp_pointer comp, int width);
+extern void Treap_insert(Treap *tmp, void* key);
+extern bool Treap_delete(Treap *tmp, void* key);
+extern void* Treap_find_data(Treap *tmp, int rnk);
+extern int Treap_find_rnk(Treap *tmp, void* key);
+extern void* Treap_find_pre(Treap *tmp, void* key);
+extern void* Treap_find_nxt(Treap *tmp, void* key);
+extern void* Treap_free(Treap *tmp);
+
+static void solve(Treap* my_treap);
+int comp(const void* a, const void *b);
---
+++ Task 7/2019201422李林豇/treap.c
@@ -0,0 +1,200 @@
+#include"treap.h"
+
+static void update(t_node *tmp)
+{
+	tmp->siz = (tmp->lc==NULL ? 0:tmp->lc->siz) + (tmp->rc==NULL ? 0:tmp->rc->siz) + tmp->cnt;
+}
+
+static t_node *new_node()
+{
+	t_node *tmp = (t_node*)malloc(sizeof(t_node));
+	tmp->lc = tmp->rc = NULL;
+	tmp->siz = tmp->cnt = 1;
+	tmp->rnd = rand();
+	return tmp;
+}
+
+static void free_node(t_node* tmp)
+{
+	free(tmp->data);
+	free((void*)tmp);
+}
+
+static void rotate(t_node **tmp, int k)
+{
+	t_node *v = (*tmp)->son[k^1];
+	(*tmp)->son[k^1] = v->son[k];
+	v->son[k]= (*tmp);
+	update((*tmp));
+	update((*tmp) = v);
+}
+
+static void insert(t_node **tmp,const void *key, cmp_pointer comp, int width)
+{
+	if((*tmp) == NULL)
+	{
+		(*tmp) = new_node();
+		(*tmp)->data = malloc(width);
+		memcpy((*tmp)->data, key, width);
+		return;
+	}
+	if(!comp(key, (*tmp)->data))
+	{
+		++(*tmp)->siz;
+		++(*tmp)->cnt;
+		return;
+	}
+	int d = comp(key, (*tmp)->data) > 0;
+	++(*tmp)->siz;
+	insert(&((*tmp)->son[d]), key, comp, width);
+	if((*tmp)->son[d]->rnd < (*tmp)->rnd)
+		rotate(tmp, d ^ 1);
+	return;
+}
+
+static bool del(t_node **tmp, const void *key, cmp_pointer comp)
+{
+	if((*tmp)==NULL)
+		return false;
+	if(!comp(key, (*tmp)->data))
+	{
+		if((*tmp)->cnt > 1)
+		{
+			--(*tmp)->cnt;
+			--(*tmp)->siz;
+			return true;
+		}
+		if((*tmp)->lc==NULL || (*tmp)->rc==NULL)
+		{
+			t_node* temp;
+			temp = (*tmp)->lc == NULL ? (*tmp)->rc : (*tmp)->lc;
+			free_node(*tmp);
+			*tmp = temp;
+			return true;
+		}
+		if((*tmp)->lc->rnd < (*tmp)->rc->rnd)
+			rotate(tmp, 1);
+		else
+			rotate(tmp, 0);
+		return del(tmp, key, comp);
+	}
+	int d = comp(key, (*tmp)->data) > 0;
+	if(!del(&((*tmp)->son[d]), key, comp))
+		return false;
+	--(*tmp)->siz;
+	return true;
+}
+
+static void* find_key(t_node *tmp, int rnk, cmp_pointer comp)
+{
+	if(tmp->siz < rnk)
+		return NULL;
+	while(1)
+	{
+		if(rnk > (tmp->lc==NULL ? 0:tmp->lc->siz) + tmp->cnt)
+			rnk -= (tmp->lc==NULL ? 0:tmp->lc->siz) + tmp->cnt, tmp = tmp->rc;
+		else
+		{
+			if(rnk <= (tmp->lc==NULL ? 0:tmp->lc->siz))
+				tmp = tmp->lc;
+			else
+				return tmp->data;
+		}
+	}
+}
+
+static int find_rnk(t_node *tmp, const void *key, cmp_pointer comp)
+{
+	int rnk = 0;
+	while(tmp != NULL)
+	{
+		if(!comp(key, tmp->data))
+			return rnk + (tmp->lc==NULL ? 0:tmp->lc->siz);
+		if(comp(key, tmp->data) > 0)
+			rnk += (tmp->lc==NULL ? 0:tmp->lc->siz) + tmp->cnt, tmp = tmp->rc;
+		else
+			tmp = tmp->lc;
+	}
+	return rnk;
+}
+
+
+static void* find_pre(t_node *tmp, const void* key, cmp_pointer comp)
+{
+	void* pre = NULL;
+	while(tmp != NULL)
+	{
+		if(comp(key, tmp->data)<=0)
+			tmp = tmp->lc;
+		else
+			pre = tmp->data, tmp = tmp->rc;
+	}
+	return pre;
+}
+
+static void* find_nxt(t_node *tmp, const void* key, cmp_pointer comp)
+{
+	void* nxt = NULL;
+	while(tmp != NULL)
+	{
+		if(comp(key, tmp->data)>=0)
+			tmp = tmp->rc;
+		else
+			nxt = tmp->data, tmp = tmp->lc;
+	}
+	return nxt;
+}
+
+static void free_tree(t_node *tmp)
+{
+	if(tmp==NULL)
+		return;
+	free(tmp->data);
+	free_tree(tmp->lc);
+	free_tree(tmp->rc);
+	free(tmp);
+}
+
+Treap* Treap_build(cmp_pointer comp, int width)
+{
+	Treap* tmp = malloc(sizeof(Treap));
+	tmp->comp = comp;
+	tmp->root = NULL;
+	tmp->width = width;
+	return tmp;
+}
+
+void Treap_insert(Treap *tmp, void* key)
+{
+	insert(&(tmp->root), key, tmp->comp, tmp->width);
+}
+
+bool Treap_delete(Treap *tmp, void* key)
+{
+	return del(&(tmp->root), key, tmp->comp);
+}
+
+void* Treap_find_data(Treap *tmp, int rnk)
+{
+	return find_key(tmp->root, rnk, tmp->comp);
+}
+
+int Treap_find_rnk(Treap *tmp, void* key)
+{
+	return find_rnk(tmp->root, key, tmp->comp);
+}
+
+void* Treap_find_pre(Treap *tmp, void* key)
+{
+	return find_pre(tmp->root, key, tmp->comp);
+}
+
+void* Treap_find_nxt(Treap *tmp, void* key)
+{
+	return find_nxt(tmp->root, key, tmp->comp);
+}
+
+void* Treap_free(Treap *tmp)
+{
+	free_tree(tmp->root);
+}
