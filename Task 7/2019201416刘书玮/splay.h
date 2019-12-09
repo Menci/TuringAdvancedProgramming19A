5d45afa147f5a5423f01cdf9a069bf34bc4a6586
Liu shui <55971939+wdmmsyf@users.noreply.github.com>
2019-12-09 16:20:43 +0800

    Task7 (#159)
    * w
    
    * w
    
    * w
    
    * w
    
    * task7
    
    * w

---
+++ Task 7/2019201416刘书玮/splay.h
@@ -0,0 +1,21 @@
+struct _node
+{
+	int count;
+	int size;
+	int value;
+	struct _node* child[2];
+	struct _node* parent;
+};
+struct _splay
+{
+	int size;
+	struct _node *root;
+};
+extern void _splay_destory(struct _splay*);
+extern struct _splay* _splay_build();
+extern void _splay_insert(struct _splay*, int);
+extern void _splay_erase (struct _splay*, int);
+extern int  _splay_kth   (struct _splay*, int);
+extern int  _splay_rank  (struct _splay*, int);
+extern struct _node* _splay_pre(struct _splay*);
+extern struct _node* _splay_nex(struct _splay*);
