0480972669d2d1b6305553319c9242b0dd9f7d6d
Treeloveswater <32383826+Treeloveswater@users.noreply.github.com>
2019-12-09 00:18:04 -0800

    Task7 (#162)
    * 882
    
    * 325
    
    * 222

---
+++ Task 7/2019201418/链接时打桩/mymalloc.c
@@ -0,0 +1,18 @@
+#ifdef LINKTIME
+#include<stdio.h>
+#include<malloc.h>
+
+void *__real_malloc(size_t size);
+void __real_free(void *ptr);
+void *__wrap_malloc( size_t size )
+{
+	void *ptr = __real_malloc( size );
+	printf(" malloc(%d) = %p\n" , (int)size , ptr );
+	return ptr;
+}
+void __wrap_free( void *ptr )
+{
+	__real_free(ptr);
+	printf(" free(%p)\n ", ptr );
+}
+#endif
