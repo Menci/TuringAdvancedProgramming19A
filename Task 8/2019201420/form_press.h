d942d8ab3fc53655f93d6b9e59833ff6f82c06c2
izumihanako <49814623+izumihanako@users.noreply.github.com>
2020-01-03 13:24:35 +0800

    补task7 (#169)
    * double !!!
    
    * 酸辣小口花
    
    * 没有站姿我要死了
    
    * 考试结束，考试结束，请考生立即停笔，若有考生继续作答，监考员应及时制止并予以违规处理
    
    * 收购二手电子产品，手机电脑（含二合一）电脑配件外设均可，1元/g可议价。（本交易仅支持同层寝室交易，支持自提，快递）
    
    * 收购二手电子产品，收电脑，收手机，收平板，收耳机，最低一元一克起！
    
    * delete test
    
    * 收购二手电子产品，收电脑，收手机，收平板，收耳机，最低一元一克起！
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete Readme.md
    
    * a
    
    * ye

---
+++ Task 8/2019201420/form_press.h
@@ -0,0 +1,9 @@
+#ifndef _FORM_PRESS_H_
+#define _FORM_PRESS_H_
+
+#include <string.h>
+#include <stdlib.h>
+void press_go( int n , int k , void *data , void *form , void *fdata ) ;
+static void dfs( int u ) ;
+
+#endif
---
+++ Task 8/2019201420/write_bin.h
@@ -0,0 +1,7 @@
+#ifndef _WRITE_BIN_
+#define _WRITE_BIN_
+
+#include <stdio.h>
+void write_to_bin( void *fm , int sfm , void *fd , int sfd ) ;
+
+#endif
